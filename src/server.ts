4fa53d25ef04b3d1495ab2270ebb26d72137e7a7
Alejandro Cabezas <alexcabezas0705@gmail.com>
2025-01-10 17:07:16 +0100

    Add simple Express server

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response } from "express";
+
+export default class Server {
+    start(): void {
+        const app: Express = express();
+        const port = process.env.PORT || 3000;
+
+        app.listen(port, () => {
+            console.log(`[server]: Server is running at http://localhost:${port}`);
+        });
+
+        app.get("/", (req: Request, res: Response) => {
+            res.send("Express + TypeScript Server");
+        });
+    }
+}
+
+
+
+
+
+
+
