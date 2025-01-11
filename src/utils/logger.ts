8f64afec82f6fe4425326c611b2cc0af54f5acf0
Alejandro Cabezas <alexcabezas0705@gmail.com>
2025-01-11 12:23:20 +0100

    Add more sophisticated logs

---
+++ src/utils/logger.ts
@@ -0,0 +1,16 @@
+import winston, {format} from "winston";
+
+
+const {printf} = format
+const customFormat = printf(({level, message, timestamp}) => {
+    return `${timestamp} [${level.toUpperCase()}] ${message}`;
+});
+
+export const Logger = winston.createLogger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        customFormat,
+        winston.format.colorize({all: true})
+    ),
+})
