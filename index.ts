f0c90ab9ee7573ed23422d3d3b5f7729c3547fde
Alejandro Cabezas <alexcabezas0705@gmail.com>
2025-01-18 16:31:56 +0100

    Remove typo

--- index.ts
+++ index.ts
@@ -4,5 +4,3 @@ import { Logger } from './src/utils/Logger'
 Server.start()
     .then(() => Logger.info('[server] Server ready!'))
     .catch(err => Logger.error(`[server] Error while trying to start server: ${err}`))
-
-console.log("aaaaa")
