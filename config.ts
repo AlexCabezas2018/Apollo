f381c3e4da8a4216b2745e5b64638a39b86b19a3
Alejandro Cabezas <alexcabezas0705@gmail.com>
2025-01-18 16:16:03 +0100

    Rerun

--- config.ts
+++ config.ts
@@ -1,22 +1,22 @@
 import 'dotenv/config'
 
 interface ConfigAttributes {
-  applicationId: string
-  publicKey: string
-  botToken: string
+    applicationId: string
+    publicKey: string
+    botToken: string
 }
 
 const evaluate = (value: any, isRequired: boolean, defValue?: any): string => {
-  if (value === undefined || value === '') {
-    if (isRequired) throw new Error('Missing required properties')
-    else return defValue
-  }
+    if (value === undefined || value === '') {
+        if (isRequired) throw new Error('Missing required properties')
+        else return defValue
+    }
 
-  return value
+    return value
 }
 
 export const Config: ConfigAttributes = {
-  applicationId: evaluate(process.env.APPLICATION_ID, true),
-  publicKey: evaluate(process.env.PUBLIC_KEY, true),
-  botToken: evaluate(process.env.BOT_TOKEN, true)
+    applicationId: evaluate(process.env.APPLICATION_ID, true),
+    publicKey: evaluate(process.env.PUBLIC_KEY, true),
+    botToken: evaluate(process.env.BOT_TOKEN, true)
 }
