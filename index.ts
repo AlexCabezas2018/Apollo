151f64425ae8f0f5aebaa7c75e02f4904ec6cbc7
Alejandro Cabezas <alexcabezas0705@gmail.com>
2025-01-18 16:41:13 +0100

    Add bad formatting

--- index.ts
+++ index.ts
@@ -4,3 +4,5 @@ import { Logger } from './src/utils/Logger'
 Server.start()
     .then(() => Logger.info('[server] Server ready!'))
     .catch(err => Logger.error(`[server] Error while trying to start server: ${err}`))
+
+       console.log("Just a test")
