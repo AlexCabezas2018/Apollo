3102fdf6ad2be90aaa1d83699201c579759a7576
Alejandro Cabezas Garríguez <43186051+alexcabezasg@users.noreply.github.com>
2025-01-19 13:00:33 +0100

    Add pause command (#55)

--- src/commands/AvailableCommands.ts
+++ src/commands/AvailableCommands.ts
@@ -2,9 +2,11 @@ import Command from './Command'
 import PlayCommand from './PlayCommand'
 import HelloCommand from './HelloCommand'
 import StopCommand from './StopCommand'
+import PauseCommand from "./PauseCommand";
 
 export const COMMANDS = new Map<string, Command>([
     ['hello', new HelloCommand()],
     ['play', new PlayCommand()],
-    ['stop', new StopCommand()]
+    ['stop', new StopCommand()],
+    ['pause', new PauseCommand()]
 ])
