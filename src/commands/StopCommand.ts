dc82ab4c162ef1c125e9eebee54bfedabd24074d
Alejandro Cabezas Garríguez <43186051+alexcabezasg@users.noreply.github.com>
2025-01-19 14:47:47 +0100

    Extract hardcoded messages (#62)

--- src/commands/StopCommand.ts
+++ src/commands/StopCommand.ts
@@ -2,11 +2,12 @@ import { Client, CommandInteraction, MessageFlagsBitField } from 'discord.js'
 import Command from './Command'
 import AudioPlayers from '../audioplayer/AudioPlayers'
 import { getVoiceConnection } from '@discordjs/voice'
+import { Messages, MessageType } from "../utils/Messages";
 
 export default class StopCommand extends Command {
     async execute(client: Client, interaction: CommandInteraction): Promise<void> {
         if ((interaction.guild == null) || interaction.guildId == null || (interaction.member == null)) {
-            await interaction.reply('Unexpected problem. Talk to a bot admin.')
+            await interaction.reply(Messages.get(MessageType.UNEXPECTED_ERROR))
             return
         }
 
@@ -17,7 +18,7 @@ export default class StopCommand extends Command {
 
         if (((voiceConnection != null) && voiceConnection.joinConfig.channelId != channel.id) || !channel) {
             await interaction.reply({
-                content: "You can't stop me if you are not in a voice channel!",
+                content: Messages.get(MessageType.USER_NOT_IN_VOICE_CHANNEL),
                 flags: MessageFlagsBitField.Flags.Ephemeral
             })
             return
@@ -26,13 +27,13 @@ export default class StopCommand extends Command {
         const audioPlayer = AudioPlayers.getInstance().getPlayer(interaction.guildId)
         if ((audioPlayer == null) || !audioPlayer.stop()) {
             await interaction.reply({
-                content: 'Nothing to stop! Try /play [url]',
+                content: Messages.get(MessageType.STOP_COMMAND_NOTHING_TO_STOP),
                 flags: MessageFlagsBitField.Flags.Ephemeral
             })
 
             return
         }
 
-        await interaction.reply('See you next time!')
+        await interaction.reply(Messages.get(MessageType.STOP_COMMAND_SUCCESS_RESPONSE))
     }
 }
