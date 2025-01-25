d9cd93314b6e23195cb75dca024884d33b724c93
Alejandro Cabezas <alexcabezas0705@gmail.com>
2025-01-25 12:57:36 +0100

    Remove hello command

--- src/commands/AvailableCommands.ts
+++ src/commands/AvailableCommands.ts
@@ -1,6 +1,5 @@
 import Command from './Command'
 import PlayCommand from './PlayCommand'
-import HelloCommand from './HelloCommand'
 import StopCommand from './StopCommand'
 import PauseCommand from "./PauseCommand";
 import ResumeCommand from "./ResumeCommand";
@@ -8,7 +7,6 @@ import ChangeLanguageCommand from "./ChangeLanguageCommand";
 import QueueCommand from "./QueueCommand";
 
 export const COMMANDS = new Map<string, Command>([
-    ['hello', new HelloCommand()],
     ['play', new PlayCommand()],
     ['stop', new StopCommand()],
     ['pause', new PauseCommand()],
--- src/commands/HelloCommand.ts
+++
@@ -1,9 +0,0 @@
-import Command, { CommandInput } from './Command'
-import { Messages, MessageType } from "../utils/Messages";
-
-export default class HelloCommand extends Command {
-    async run(input: CommandInput): Promise<void> {
-        const { guildPreferences, interaction } = input;
-        await interaction.reply(Messages.get(guildPreferences, MessageType.HELLO_COMMAND_SUCCESS_RESPONSE))
-    }
-}
